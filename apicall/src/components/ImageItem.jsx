8882dff900d6bb8459fa16ad989fc608df0baef9
Furkan Soylu <fsoylu567@gmail.com>
2024-05-01 14:39:36 +0300

    Return images to the screen

--- apicall/src/components/ImageItem.jsx
+++ apicall/src/components/ImageItem.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ImageItem = ( { image } ) => {
     console.log(image)
   return (
-    <div></div>
+    <div>
+        <img  className='imageListItems' src={image.urls.small} alt={image.alt_description} />
+    </div>
   )
 }
 
